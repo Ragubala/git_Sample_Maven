ce5eaef5fb5b7b65142b57714020bad65f11edd3
Ragubala <balajidx11@gmail.com>
2024-03-28 20:30:21 +0530

    modified some lines

--- java/Sample_Maven_Git.java
+++ java/Sample_Maven_Git.java
@@ -2,8 +2,10 @@
 public class Sample_Maven_Git {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hello_bala");
+		
+       System.out.println("hello_bala");
+       System.out.println("creat cart");
 	}
 
+	
 }
